044644bd65e20e781e0bb07e81fd46eb83cd0497
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2023-12-14 15:34:48 +0200

    get rid of array variable, Number type for array's elements to reduce

--- index.js
+++ index.js
@@ -4,69 +4,32 @@
 
 cancel має припиняти виконання програми, а порожній рядок не повинен сприйматися як 0 */
 
-// let inputItems = prompt('Ввести довільну кількість чисел через кому.');
-// debugger;
-// console.log(inputItems);
-
-// if (inputItems === null) {
-//   alert('Відміна.');
-// }
-
-// else if (!inputItems?.trim()) {
-//   while (!inputItems?.trim()) {
-//     if (inputItems === null) {
-//       alert('Відміна.');
-//       break;
-//     }
-//     inputItems = prompt(
-//       'Ви нічого не ввели чи ввели пусте значення. Введіть довільну кількість чисел через кому.'
-//     );
-//   }
-// } else {
-//   let array = inputItems.split(',');
-//   let isNotANumberElement = array.some((element) =>
-//     isNaN(Number(element.trim()))
-//   );
-//   let result;
-
-//   if (isNotANumberElement || !inputItems?.trim()) {
-//     while (isNotANumberElement || !inputItems?.trim()) {
-//       if (inputItems === null) {
-//         alert('Відміна.');
-//         break;
-//       }
-//       inputItems = prompt(
-//         'Серед введених значень було не число. Введіть довільну кількість чисел через кому'
-//       );
-//     }
-//   } else {
-//     array = inputItems.split(',');
-//     result = array.reduce((prev, next) => +prev + +next, 0);
-//     alert(result);
-//   }
-// }
+// Варіант 1. Лише виведення помилки
 
 // let inputItems = prompt('Ввести довільну кількість чисел через кому.');
 // debugger;
 // console.log(inputItems);
+
+// let isNotANumberElement = inputItems
+//   .split(',')
+//   .some((element) => isNaN(Number(element.trim())) || !element.trim());
 
 // if (inputItems === null) {
 //   alert('Відміна');
-// } else if (!inputItems?.trim()) {
-//   alert('Пусте значення');
-// } else {
-//   let array = inputItems.split(',');
-//   let isNotANumberElement = array.some(
-//     (element) => isNaN(Number(element.trim())) || !element.trim()
-//   );
-//   let result;
-//   if (isNotANumberElement) {
+// } else if (!inputItems?.trim() || isNotANumberElement) {
+//   if (!inputItems?.trim()) {
+//     alert('Пусте значення або не число.');
+//   } else if (isNotANumberElement) {
 //     alert('Деякі елменти не є числами');
-//   } else {
-//     result = array.reduce((prev, next) => Number(prev) + Number(next), 0);
-//     alert(result);
 //   }
+// } else {
+//   let result = parseFloat(
+//     inputItems.split(',').reduce((prev, next) => Number(prev) + Number(next), 0)
+//   );
+//   alert(`Сума введених чисел дорівнює ${result}`);
 // }
+
+// Варіант 2. Постійний запит до введення коректних даних.
 
 let inputItems = prompt('Ввести довільну кількість чисел через кому.');
 // debugger;
@@ -94,7 +57,8 @@ while (inputItems !== null && (!inputItems?.trim() || isNotANumberElement)) {
 if (inputItems === null) {
   alert('Відміна');
 } else {
-  let array = inputItems.split(',');
-  let result = parseFloat(array.reduce((prev, next) => Number(prev) + Number(next), 0));
+  let result = parseFloat(
+    inputItems.split(',').reduce((prev, next) => Number(prev) + Number(next), 0)
+  );
   alert(`Сума введених чисел дорівнює ${result}`);
 }
